e724f8bb6947e91567c308cdcab06de080c5adb7
Tom <abaotom14@gmail.com>
2020-10-24 09:14:16 +0800

    add CORS_ORIGIN_WHITELIST

--- drf-notes_project/notes/views.py
+++ drf-notes_project/notes/views.py
@@ -3,7 +3,7 @@ from rest_framework import generics
 from .models import Note
 from .serializers import NoteSerializer
 
-class NoteList(generics.ListAPIView):
+class NoteList(generics.ListCreateAPIView):
     queryset = Note.objects.all()
     serializer_class = NoteSerializer
 
