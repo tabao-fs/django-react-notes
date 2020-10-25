5ae14f21f2016af5229ea50dff0c08a0052bfea0
Tom <abaotom14@gmail.com>
2020-10-25 19:41:03 +0800

    use ListNotes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,42 @@
 import React, { Component } from 'react';
-import { Button, Container, Row, Col } from 'reactstrap'
+import { Button, Container, Row, Col } from 'reactstrap';
+
+import ListNotes from './components/ListNotes';
+
+var notes_temp = [
+  {
+    'id': 1,
+    'title': 'this is a react note',
+    'content': 'this is the content'
+  },
+  {
+    'id': 2,
+    'title': 'second note',
+    'content': 'this is the second content'
+  },
+  {
+    'id': 3,
+    'title': 'third note',
+    'content': 'this is the third content'
+  }
+]
 
 class App extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      notes: [],
+      notes: notes_temp,
       current_note_id: 0,
       is_creating: true
     }
+
+    this.handleItemClick = this.handleItemClick.bind(this);
   };
+
+  handleItemClick(id) {
+    console.log("id", id);
+  }
 
   render() {
     return (
@@ -26,7 +52,7 @@ class App extends Component {
           </Row>
           <Row>
             <Col xs="4">
-              <h3>List our notes here...</h3>
+              <ListNotes notes={this.state.notes} handleItemClick={(id) => this.handleItemClick(id)} />
             </Col>
             <Col xs="8">
               <p>Content/Editing here...</p>
